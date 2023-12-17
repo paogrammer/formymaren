6af7d4a7546db5e3b020f8e9f838f32c3ebef770
paogrammer <60766499+paogrammer@users.noreply.github.com>
2023-12-17 19:15:05 +0800

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -9,7 +9,7 @@ const Home = () => {
       {/* <h1>Home Page</h1> */}
       <div className='home-text'>
         <p className='mb-5'>Hi Maren,</p>
-        <p>In this page, I made you a tulip. It's not the best-looking nor the most realistic flower you'll ever see, but I programmed it myself. In this world, there's only one of it. Just like you--one of a kind.</p>
+        <p>In this page, I made you a tulip. It&apos;s not the best-looking nor the most realistic flower you&apos;ll ever see, but I programmed it myself. In this world, there&apos;s only one of it. Just like you--one of a kind.</p>
       </div>
       
       <div className='tulip'>
